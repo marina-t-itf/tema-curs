2f4d673f333ffc40fdf453669f6ea924f5d090ab
Marina T <toader.marinaloredana@gmail.com>
2022-05-29 11:13:22 +0300

    function in js

--- tema-3/tema_3.js
+++ tema-3/tema_3.js
@@ -1,4 +1,5 @@
-const paragraph = {
+//homework item 1 and 2
+let paragraph = {
 name1 : ' Jane Smith ',
 action1 : ' has ',
 action2 : ' is ',
@@ -11,12 +12,12 @@ phrase3 : ' to buy a Toyota Yaris',
 phrase4 : ' for a Toyota Yaris'
 };
 
-const paragraph1 = paragraph.name1 + paragraph.action1 + paragraph.prece + paragraph.phrase1 +'.';
-const paragraph2 = paragraph.prece + paragraph.action2 + paragraph.phrase2 +'.';
-const paragraph3 = paragraph.name1 + paragraph.action3 + paragraph.phrase3 +'.';
-const paragraph4 = paragraph.name1 + paragraph.action4 + paragraph.prece + paragraph.phrase3 +'.';
-const paragraph5 = paragraph.prece + paragraph.action2 + paragraph.phrase2 + paragraph.phrase4 +'.';
-const delimitation = '======================================================';
+let paragraph1 = paragraph.name1 + paragraph.action1 + paragraph.prece + paragraph.phrase1 +'.';
+let paragraph2 = paragraph.prece + paragraph.action2 + paragraph.phrase2 +'.';
+let paragraph3 = paragraph.name1 + paragraph.action3 + paragraph.phrase3 +'.';
+let paragraph4 = paragraph.name1 + paragraph.action4 + paragraph.prece + paragraph.phrase3 +'.';
+let paragraph5 = paragraph.prece + paragraph.action2 + paragraph.phrase2 + paragraph.phrase4 +'.';
+let delimitation = '======================================================';
 
 document.getElementById("demo1").innerHTML =
 paragraph1 + '<br>'+ paragraph2 + '<br>'+ paragraph3 + '<br>'+ paragraph4 + '<br>'+ paragraph5;
@@ -30,3 +31,16 @@ document.getElementById("demoDelimitation2").innerHTML = delimitation;
 
 document.getElementById("demo3").innerHTML =
 paragraph3 + '<br>'+ paragraph5 + '<br>'+ paragraph4 + '<br>'+ paragraph1 + '<br>'+ paragraph2;
+
+//homework item 3
+let paragraphLine = myParagraph ();
+document.getElementById("demoFunction").innerHTML = paragraphLine;
+
+function myParagraph() {
+    return paragraph1 + '<br>'+ paragraph2 + '<br>'+ paragraph3 + '<br>'+ paragraph4 + '<br>'+ paragraph5 +'<br>'+'<br>'+
+    delimitation +'<br>'+'<br>'+
+    paragraph2 + '<br>'+ paragraph5 + '<br>'+ paragraph3 + '<br>'+ paragraph1 +'<br>'+ paragraph4 +'<br>'+'<br>'+
+    delimitation +'<br>'+ '<br>'+
+    paragraph3 + '<br>'+ paragraph5 + '<br>'+ paragraph4 + '<br>'+ paragraph1 + '<br>'+ paragraph2;
+}
+
