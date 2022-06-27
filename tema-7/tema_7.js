804726fb8bf10825978ec7d826b41f20242d8e5a
Marina T <toader.marinaloredana@gmail.com>
2022-06-27 17:24:05 +0300

    some change homework

--- tema-7/tema_7.js
+++ tema-7/tema_7.js
@@ -12,13 +12,11 @@ let initialPositionSquare = function() {
         square.style.top = '50px';
         square.style.left = '50px';
     }
-
 };
 
 initialPositionSquare();
 
 let onSquareClick = function () {
-    
     let square = document.querySelector('.popup');
     if (window.matchMedia('(max-width: 500px)').matches) {
         square.style.position = 'fixed';
@@ -33,11 +31,8 @@ let onSquareClick = function () {
         square.style.bottom = "50px";
         square.style.right = "50px";
     } 
-    
 };
-
 
 let adjustedSquare = document.querySelector('.popup');
 adjustedSquare.addEventListener('click', onSquareClick);
-
 
