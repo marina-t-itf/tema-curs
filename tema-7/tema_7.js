906314239cdc2df68e2764b5214492a58e7aa486
Tudor Goia <mindsetcoach360@gmail.com>
2022-06-27 23:08:37 +0300

    fix home work - complicated java only - to be optimized

--- tema-7/tema_7.js
+++ tema-7/tema_7.js
@@ -1,38 +1,70 @@
 // https://www.w3schools.com/cssref/tryit.asp?filename=trycss_js_right
 
 
-let initialPositionSquare = function() {
-    let square = document.querySelector('.popup');
-    if (window.matchMedia('(max-width: 500px)').matches) {
-        square.style.position = 'fixed';
-        square.style.top = '5px';
-        square.style.left = '5px';
-    } else {
-        square.style.position = 'fixed';
-        square.style.top = '50px';
-        square.style.left = '50px';
+let square = document.querySelector('.popup');
+
+let init = function() {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+        square.style.top = "5px";
+        square.style.left = "5px";
+    }
+    if (window.matchMedia('(min-width: 601px)').matches) {
+        square.style.top = "50px";
+        square.style.left = "50px";
     }
 };
 
-initialPositionSquare();
-
-let onSquareClick = function () {
-    let square = document.querySelector('.popup');
-    if (window.matchMedia('(max-width: 500px)').matches) {
-        square.style.position = 'fixed';
-        square.style.removeProperty('top');
-        square.style.removeProperty('left');
-        square.style.bottom = "5px";
-        square.style.right = "5px";
-    } else {
-        square.style.position = 'fixed';
-        square.style.removeProperty('top');
-        square.style.removeProperty('left');
-        square.style.bottom = "50px";
-        square.style.right = "50px";
-    } 
+let onResize = function() {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+        if (square.style.top !== '') {
+            square.style.top = "5px";
+            square.style.left = "5px";
+        } else {
+            square.style.bottom = "5px";
+            square.style.right = "5px";
+        }
+    }
+    if (window.matchMedia('(min-width: 601px)').matches) {
+        if (square.style.top !== '') {
+            square.style.top = "50px";
+            square.style.left = "50px";
+        } else {
+            square.style.bottom = "50px";
+            square.style.right = "50px";
+        }
+    }
 };
 
-let adjustedSquare = document.querySelector('.popup');
-adjustedSquare.addEventListener('click', onSquareClick);
+let toggle = function() {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+        if (square.style.top !== '') {
+            square.style.removeProperty('top');
+            square.style.removeProperty('left');
+            square.style.bottom = "5px";
+            square.style.right = "5px";
+        } else {
+            square.style.removeProperty('bottom');
+            square.style.removeProperty('right');
+            square.style.top = "5px";
+            square.style.left = "5px";
+        }
+    }
+    if (window.matchMedia('(min-width: 601px)').matches) {
+        if (square.style.top !== '') {
+            square.style.removeProperty('top');
+            square.style.removeProperty('left');
+            square.style.bottom = "50px";
+            square.style.right = "50px";
+        } else {
+            square.style.removeProperty('bottom');
+            square.style.removeProperty('right');
+            square.style.top = "50px";
+            square.style.left = "50px";
+        }
+    }
+};
+
+init();
+square.addEventListener('click', toggle);
+window.addEventListener('resize', onResize, true);
 
