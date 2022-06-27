5448f525e350d4cc0638a3e55f8347df1bfc9576
Marina T <toader.marinaloredana@gmail.com>
2022-06-27 14:21:34 +0300

    matchMedia subject 2

--- tema-7/tema_7.js
+++ tema-7/tema_7.js
@@ -3,22 +3,41 @@
 
 let initialPositionSquare = function() {
     let square = document.querySelector('.popup');
-    square.style.position = 'fixed';
-    square.style.top = '50px';
-    square.style.left = '50px';
-}
+    if (window.matchMedia('(max-width: 500px)').matches) {
+        square.style.position = 'fixed';
+        square.style.top = '5px';
+        square.style.left = '5px';
+    } else {
+        square.style.position = 'fixed';
+        square.style.top = '50px';
+        square.style.left = '50px';
+    }
+
+};
 
 initialPositionSquare();
 
 let onSquareClick = function () {
+    
     let square = document.querySelector('.popup');
-    square.style.position = 'fixed';
-    square.style.removeProperty('top');
-    square.style.removeProperty('left');
-    square.style.bottom = "50px";
-    square.style.right = "50px";
+    if (window.matchMedia('(max-width: 500px)').matches) {
+        square.style.position = 'fixed';
+        square.style.removeProperty('top');
+        square.style.removeProperty('left');
+        square.style.bottom = "5px";
+        square.style.right = "5px";
+    } else {
+        square.style.position = 'fixed';
+        square.style.removeProperty('top');
+        square.style.removeProperty('left');
+        square.style.bottom = "50px";
+        square.style.right = "50px";
+    } 
+    
 };
 
 
 let adjustedSquare = document.querySelector('.popup');
 adjustedSquare.addEventListener('click', onSquareClick);
+
+
