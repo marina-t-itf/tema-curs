ea8da03311c9f8973dc14594278e112fec7d7a28
Marina T <toader.marinaloredana@gmail.com>
2022-07-01 15:46:36 +0300

    create folders bootstrap and axios

---
+++ tema-8/tema_8.js
@@ -0,0 +1,23 @@
+// let crew = {
+//     initialize: function() {
+
+//         axios
+//             .get('https://api.spacexdata.com/v4/crew')
+//             .then(crew.onAxiosFinished)
+//             .catch(function(error) {
+//                 // handle error
+//                 console.log(error);
+//             })
+//         ;
+
+//         popupHandling.popupInit();
+
+//     },
+
+//     onAxiosFinished: function(response) {
+//         let launchData = response.data;
+//         // TODO stuff here
+//     },
+// };
+
+// crew.initialize();
