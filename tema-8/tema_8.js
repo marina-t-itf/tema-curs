f85ec1e2587a08de7ddb705bbde6dcda3ab4ab1c
Marina T <toader.marinaloredana@gmail.com>
2022-07-02 15:59:42 +0300

    create popupTable and close boton

--- tema-8/tema_8.js
+++ tema-8/tema_8.js
@@ -49,35 +49,101 @@
 // incercare creare tabel 2
 
 
-function onClickTable() {
-    // creates a <table> element and a <tbody> element
-     let tbl = document.createElement("table");
-     let tblBody = document.createElement("tbody");
+// function onClickTable() {
+//     // creates a <table> element and a <tbody> element
+//      let tbl = document.createElement("table");
+//      let tblBody = document.createElement("tbody");
    
-     // creating all cells
-     for (let i = 0; i < 1; i++) {
-       // creates a table row
-       let row = document.createElement("tr");
+//      // creating all cells
+//      for (let i = 0; i < 1; i++) {
+//        // creates a table row
+//        let row = document.createElement("tr");
    
-       for (let j = 0; j < 3; j++) {
-         // Create a <td> element and a text node, make the text
-         // node the contents of the <td>, and put the <td> at
-         // the end of the table row
-         let cell = document.createElement("td");
-         let cellText = document.createTextNode("cell in row "+i+", column "+j);
-         cell.appendChild(cellText);
-         row.appendChild(cell);
+//        for (let j = 0; j < 3; j++) {
+//          // Create a <td> element and a text node, make the text
+//          // node the contents of the <td>, and put the <td> at
+//          // the end of the table row
+//          let cell = document.createElement("td");
+//          let cellText = document.createTextNode("cell in row "+i+", column "+j);
+//          cell.appendChild(cellText);
+//          row.appendChild(cell);
         
-       }
+//        }
    
-       // add the row to the end of the table body
-       tblBody.appendChild(row);
-     }
+//        // add the row to the end of the table body
+//        tblBody.appendChild(row);
+//      }
    
-     // put the <tbody> in the <table>
-     tbl.appendChild(tblBody);
-     // appends <table> into <body>
-     document.body.appendChild(tbl);
-     // sets the border attribute of tbl to '2'
-     tbl.setAttribute("border", "2");
-   }
+//      // put the <tbody> in the <table>
+//      tbl.appendChild(tblBody);
+//      // appends <table> into <body>
+//      document.body.appendChild(tbl);
+//      // sets the border attribute of tbl to '2'
+//      tbl.setAttribute("border", "2");
+//    }
+
+
+
+let popupCloseButton = document.querySelector('.closeButton > span');
+
+let popupClose = function () {
+    // hide the popup
+    let tablePopup = document.querySelector('.tableWindow');
+    tablePopup.style.display = 'none';
+};
+
+popupCloseButton.addEventListener('click', popupClose);
+
+let showPopup = document.querySelector('.btn');
+
+let popupOpen = function () {
+    // show the popup
+    let tablePopup = document.querySelector('.tableWindow');
+    tablePopup.style.display = 'block';
+};
+
+showPopup.addEventListener('click', popupOpen);
+
+axios
+    .get('https://api.spacexdata.com/v4/crew')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+
+        // only the data about crew
+        // console.log(response.data);
+
+        // only data of one member of the crew
+        // console.log(response.data[0]);
+
+        // only the name of one member of the crew
+        // console.log(response.data[0].name);
+
+
+        // all names for the crew members in console
+        // for (i=0; i<response.data.length; i++) {
+        //     console.log(response.data[i].name);
+        // }
+
+
+        let bringName = function() {
+            for (i=0; i<response.data.length; i++) {
+                console.log(response.data[i].name);
+
+                let bringRow = document.querySelector('.longRow');
+                bringRow.style.color= 'orange';
+
+                let personName = document.querySelector('.name');
+                personName.innerText = response.data[0].name;
+            }
+        }
+
+        bringName();
+
+
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+;
