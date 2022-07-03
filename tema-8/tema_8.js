cb9dcdda8c59c00fd4ceb03bd016f35c323d6a0f
Marina T <toader.marinaloredana@gmail.com>
2022-07-03 13:24:39 +0300

    Create tabel in js and complet them with axios data

--- tema-8/tema_8.js
+++ tema-8/tema_8.js
@@ -1,88 +1,3 @@
-// let crew = {
-//     initialize: function() {
-
-//         axios
-//             .get('https://api.spacexdata.com/v4/crew')
-//             .then(crew.onAxiosFinished)
-//             .catch(function(error) {
-//                 // handle error
-//                 console.log(error);
-//             })
-//         ;
-
-//         popupHandling.popupInit();
-
-//     },
-
-//     onAxiosFinished: function(response) {
-//         let launchData = response.data;
-//         // TODO stuff here
-//     },
-// };
-
-// crew.initialize();
-
-//incercare creare tabel 1
-
-// let onClickTable = function() {
-//     let x = document.createElement("TABLE");
-//     x.setAttribute("id", "myTable");
-//     document.body.appendChild(x);
-  
-//     let y = document.createElement("TR");
-//     y.setAttribute("id", "myTr");
-//     document.getElementById("myTable").appendChild(y);
-  
-//     let z = document.createElement("TD");
-//     let t = document.createTextNode("cell");
-//     z.appendChild(t);
-//     document.getElementById("myTr").appendChild(z);
-
-//     // let table = document.createElement("TABLE");
-//     // let row = table.insertRow(0);
-//     // let cell1 = row.insertCell(0);
-//     // let cell2 = row.insertCell(1);
-//     // cell1.innerHTML = "NEW CELL1";
-//     // cell2.innerHTML = "NEW CELL2";
-// }
-
-// incercare creare tabel 2
-
-
-// function onClickTable() {
-//     // creates a <table> element and a <tbody> element
-//      let tbl = document.createElement("table");
-//      let tblBody = document.createElement("tbody");
-   
-//      // creating all cells
-//      for (let i = 0; i < 1; i++) {
-//        // creates a table row
-//        let row = document.createElement("tr");
-   
-//        for (let j = 0; j < 3; j++) {
-//          // Create a <td> element and a text node, make the text
-//          // node the contents of the <td>, and put the <td> at
-//          // the end of the table row
-//          let cell = document.createElement("td");
-//          let cellText = document.createTextNode("cell in row "+i+", column "+j);
-//          cell.appendChild(cellText);
-//          row.appendChild(cell);
-        
-//        }
-   
-//        // add the row to the end of the table body
-//        tblBody.appendChild(row);
-//      }
-   
-//      // put the <tbody> in the <table>
-//      tbl.appendChild(tblBody);
-//      // appends <table> into <body>
-//      document.body.appendChild(tbl);
-//      // sets the border attribute of tbl to '2'
-//      tbl.setAttribute("border", "2");
-//    }
-
-
 
 let popupCloseButton = document.querySelector('.closeButton > span');
 
@@ -126,24 +41,33 @@ axios
         // }
 
 
-        let bringName = function() {
-            // let personName = document.querySelector('.rowTabel1');
+        // let bringName = function() {
+        let initialTabel = document.querySelectorAll('.myTabel');
+        let myCreateFunction = function(initialTabel) {
             for (i=0; i<response.data.length; i++) {
                 console.log(response.data[i].name);
-            //     let table = document.getElementsByClassName('table');
-            //     let row = table.insertRow(0);
-            //     let cell1 = row.insertCell(0);
-            //     let cell2 = row.insertCell(1);
-                    
-            //     cell1.innerText = response.data[i].name;
 
-                let personName = document.querySelector('.rowTabel-'+ (i+1));
-                personName.innerText = response.data[i].name;
+                let tableRef = document.getElementById('myTabel');
+                let row = tableRef.insertRow(i);
+                let cell1 = row.insertCell();
+                let cell2 = row.insertCell();
+                let cell3 = row.insertCell();
+                let cell4 = row.insertCell();
+                cell1.innerHTML = i+1;
+                cell2.innerHTML = response.data[i].name;;
+                cell3.innerHTML = " ";
+                cell4.innerHTML = "NEW CELL4";
+                if(i% 2 === 1) {
+                    row.className = 'Row table-active';
+                }
             }
+                // let personName = document.querySelector('.rowTabel-'+ (i+1));
+                // personName.innerHTML = response.data[i].name;
         }
-
-        bringName();
-
+        // }
+    
+        // bringName();
+        myCreateFunction();
 
     })
     .catch(function (error) {
@@ -152,15 +76,19 @@ axios
     })
 ;
 
-
-// let myCreateFunction = function() {
-//     let initialTabel = document.querySelectorAll('.myTabel');
-//     let table = document.getElementById('myTable');
-//     let row = table.insertRow();
+// let initialTabel = document.querySelectorAll('.myTabel');
+// let myCreateFunction = function(initialTabel) {
+    
+//     let tableRef = document.getElementById('myTabel');
+//     let row = tableRef.insertRow(-1);
 //     let cell1 = row.insertCell();
 //     let cell2 = row.insertCell();
+//     let cell3 = row.insertCell();
+//     let cell4 = row.insertCell();
 //     cell1.innerHTML = "NEW CELL1";
 //     cell2.innerHTML = "NEW CELL2";
+//     cell3.innerHTML = " ";
+//     cell4.innerHTML = "NEW CELL4";
 // }
 
-// myCreateFunction();
+
