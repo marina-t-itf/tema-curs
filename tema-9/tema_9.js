1cab9f888e7671b2df22fd7d7f45498a327d8d4b
Marina T <toader.marinaloredana@gmail.com>
2022-07-08 12:46:08 +0300

    set reference date and current date

---
+++ tema-9/tema_9.js
@@ -0,0 +1,8 @@
+let theCurrentDate = new Date();
+console.log(theCurrentDate);
+
+document.getElementById('currentDate').innerHTML= theCurrentDate;
+
+let referenceDate = new Date(2021, 06, 01);
+console.log(referenceDate);
+
