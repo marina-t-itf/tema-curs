c844af5d2897bcbcd30dbe3200fe5eef24ced164
Marina T <toader.marinaloredana@gmail.com>
2022-07-21 13:21:52 +0300

    add crew name and style page

--- tema-10/tema_10.js
+++ tema-10/tema_10.js
@@ -3,20 +3,38 @@ axios
     .then(function (response) {
         console.log(response);
 
-        let initialDiv = document.querySelectorAll('.crewImages');
+        // let initialDiv = document.querySelectorAll('.crewImages');
         let myCreateFunction = function(initialDiv) {
             for (i=0; i<response.data.length; i++) {
                 console.log(response.data[i].image);
+                console.log(response.data[i].name);
 
-                let memberImage = document.getElementsByClassName('.crewImages');
+                let newDiv = document.createElement('div');
                 let newImage = document.createElement('IMG');
-               
+                let initialDiv = document.getElementById('crew');
+                let newDivImage = document.createElement('div');
+                
+
+                newDiv.innerHTML = response.data[i].name;
                 newImage.innerHTML = response.data[i].image;
                 newImage.setAttribute("src", response.data[i].image);
                 newImage.setAttribute("width", "300");
                 newImage.setAttribute("height", "300");
+                newDiv.classList.add('nameMember');
+                newDiv.setAttribute("width", "300");
+                newDiv.setAttribute("height", "300");
+                newDivImage.setAttribute("width", "300");
+                newDivImage.setAttribute("height", "300");
+                newDivImage.classList.add('imageDiv');
                 
-                document.body.appendChild(newImage);
+                newDivImage.appendChild(newDiv);
+                
+                // document.body.appendChild(newImage);
+                // document.getElementsByClassName("crewImages").style.background = "url(newImage)";
+                // document.body.appendChild(newDiv);
+                initialDiv.appendChild(newDivImage).appendChild(newImage);
+                // initialDiv.appendChild(newDiv);
+                
             }
         }
         myCreateFunction();
