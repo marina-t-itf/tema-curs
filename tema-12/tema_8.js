76ebe823e833fbe557ea70aab49db5c01ef5e848
Marina T <toader.marinaloredana@gmail.com>
2022-08-11 20:51:25 +0300

    create table with all launches data

--- tema-12/tema_8.js
+++ tema-12/tema_8.js
@@ -1,5 +1,33 @@
+
+let popupCloseButton = document.querySelector('.closeButton > span');
+let showButton = document.querySelector('.btn');
+
+let popupClose = function () {
+    // hide the popup
+    let tablePopup = document.querySelector('.tableWindow');
+    tablePopup.style.display = 'none';
+
+    let launchButton = document.querySelector('.btn');
+    launchButton.style.display = 'block';
+};
+
+popupCloseButton.addEventListener('click', popupClose);
+
+let showPopup = document.querySelector('.btn');
+
+let popupOpen = function () {
+    // show the popup
+    let tablePopup = document.querySelector('.tableWindow');
+    tablePopup.style.display = 'block';
+
+    let launchButton = document.querySelector('.btn');
+    launchButton.style.display = 'none';
+};
+
+showPopup.addEventListener('click', popupOpen);
+
 axios
-    .get('https://api.spacexdata.com/v4/launches')
+    .get('https://api.spacexdata.com/v4/crew')
     .then(function (response) {
         // handle success
         console.log(response);
@@ -24,7 +52,7 @@ axios
         let initialTable = document.querySelectorAll('.myTabel');
         let myCreateFunction = function(initialTable) {
             for (i=0; i<response.data.length; i++) {
-                console.log(response.data[i].date_utc);
+                console.log(response.data[i].name);
 
                 let tableRef = document.getElementById('myTabel');
                 let row = tableRef.insertRow(i);
@@ -34,7 +62,7 @@ axios
                 let cell2 = row.insertCell();
                 let cell3 = row.insertCell();
                 let cell4 = row.insertCell();
-                cell2.innerHTML = response.data[i].date_utc;;
+                cell2.innerHTML = response.data[i].name;;
                 cell3.innerHTML = " ";
                 cell4.innerHTML = "NEW CELL4";
                 if(i% 2 === 1) {
@@ -55,3 +83,20 @@ axios
         console.log(error);
     })
 ;
+
+// let initialTabel = document.querySelectorAll('.myTabel');
+// let myCreateFunction = function(initialTabel) {
+    
+//     let tableRef = document.getElementById('myTabel');
+//     let row = tableRef.insertRow(-1);
+//     let cell1 = row.insertCell();
+//     let cell2 = row.insertCell();
+//     let cell3 = row.insertCell();
+//     let cell4 = row.insertCell();
+//     cell1.innerHTML = "NEW CELL1";
+//     cell2.innerHTML = "NEW CELL2";
+//     cell3.innerHTML = " ";
+//     cell4.innerHTML = "NEW CELL4";
+// }
+
+
