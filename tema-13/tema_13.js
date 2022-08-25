2993dbc980939df4bc38387af46ba4b74cd1a769
Marina T <toader.marinaloredana@gmail.com>
2022-08-25 18:57:36 +0300

    added SpaceX crew images in html page and styled

--- tema-13/tema_13.js
+++ tema-13/tema_13.js
@@ -68,3 +68,48 @@ let sortArray = function(){
 }
 
 sortArray(unsortedArray);
+
+// sort SpaceX crew image and name:
+axios
+    .get('https://api.spacexdata.com/v4/crew')
+    .then(function (response) {
+        console.log(response);
+
+        let myCreateFunction = function() {
+            for (i=0; i<response.data.length; i++) {
+                console.log(response.data[i].image);
+                console.log(response.data[i].name);
+
+                let newDiv = document.createElement('div');
+                let newImage = document.createElement('IMG');
+                let initialDiv = document.getElementById('crew');
+                let newDivImage = document.createElement('div');
+                
+                newDiv.innerHTML = response.data[i].name;
+                newImage.innerHTML = response.data[i].image;
+
+                newImage.setAttribute('src', response.data[i].image);
+                // newImage.setAttribute('width', '300');
+                // newImage.setAttribute('height', '300');
+                // newDiv.setAttribute('width', '300');
+                // newDiv.setAttribute('height','300');
+                // newDivImage.setAttribute('width', '300');
+                // newDivImage.setAttribute('height', '300');
+
+                newDiv.classList.add('nameMember');
+                newDivImage.classList.add('imageDiv');
+
+                newDivImage.style.backgroundImage = `url(${response.data[i].image})`;
+                
+                newDivImage.appendChild(newDiv);
+                initialDiv.appendChild(newDivImage);
+                // .appendChild(newImage);
+            }
+        }
+        myCreateFunction();
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+;
